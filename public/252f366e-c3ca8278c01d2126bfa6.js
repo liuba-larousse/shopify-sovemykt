7c5eb8cf392f56210bcaec89c2dd2b66af0efa78
liuba-larousse <liubovkapitulskaya@gmail.com>
2021-01-14 11:50:55 -0600

    public addaed

---
+++ public/252f366e-c3ca8278c01d2126bfa6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{NIcq:function(t,n,a){"use strict";a.d(n,"a",(function(){return i}));var c=a("Lnxd");function i(t){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm-2 16l-4-4 1.41-1.41L10 14.17l6.59-6.59L18 9l-8 8z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-c3ca8278c01d2126bfa6.js.map
