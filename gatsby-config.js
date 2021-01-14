61875034f0bb278e073e4370ccfa0dde788a17db
liuba-larousse <liubovkapitulskaya@gmail.com>
2021-01-14 09:11:12 -0600

    gatsby-ssr.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,15 +31,7 @@ module.exports = {
         component: require.resolve(`./src/components/Layout`),
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        // This path is relative to the root of the site.
-      },
-    },
+
     {
       resolve: `gatsby-source-shopify`,
       options: {
@@ -83,7 +75,21 @@ module.exports = {
       options: {
         spreadsheetId: '1_msuzOCuoxcv211ivo1JtTtomT0c-Bk64Pp3QRqXQ6g',
         // apiKey: process.env.GOOGLE_API_KEY,
-        credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS),
+        credentials: {
+          type: 'service_account',
+          project_id: 'sovemykt',
+          private_key_id: '64c6d0edb5e07cec107c478d18203acec21b0ebc',
+          private_key:
+            '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCroloZ6iokSriu\nGu7cbq1RCwtUJGTi/2W/WYqyzAw9ub7tDntRt2EH47um64OwTzXVlbIgYy/6UaSQ\nF7cObcvXRaFTXYiiCi3Q6pL4CwEac/wRN0xYSUiYyqgpykYSe4k9esV3I+v76pRn\nId1np9n1jAh/pkva8PVHzAfstj5kv+PDJHqXAWTPDiu0EDqPjjKfMMvn+mu1B4y+\nfJ0SNc4r3X00QV6ObznNwWXRy1kWiyknbfahZ7j0ZtdnZmbCjDdns3gyjj+d57KI\ng8/jsNcV7h6RswYwB0//Wnx8tdnqES32w4Rq9JJ61szb+wbpxMqTNiAE2g6TlJ4j\noeNeFiAxAgMBAAECgf8YU8YoeHYS9/IQu3do67TJx+uKAZ6URQy5NxxxqQylafel\n60L9LZMliyHpYlPaYIRleIk0oEgsoIppmPLrqu2CulvaUIwx2T+AplFTIDwsXx+k\ndZ/KUXRB6sDGmVLDAr+VrhpPEezzNbpldh403CgIpdCOkHFUlxvj7L5XYTRSN8k2\njCYZlPFX2x7dFh6/bkOzwqvygDAEGxuju1hK/e4HRyzjG2tuXCrQpZZw3qS89elL\nc7r2IaFu/4pUU+adEDytc9ZV/NRd8+CgAoti2bzf+DkRn4oeWEcWqK0YxBdSiuPV\nb0jpnHQxW75b0GWMVg4UY//a+cVGEZNb+C6dOQkCgYEA8NGWk9ZBfiXijFtPxSBl\n+lipbROCnqEwfVm1/kAkyqkGBdSQHsLFX01C48oq9kW6TTjobHnCX+iK8fmuOctA\nf7t31wqOElpKFA2yI9vQUm1n85bwQDP5RJno93lBHkH36T1xhcEII4PSnqJtN5xM\niF6gFZaVcS3dmcQ/WnVZQ68CgYEAtnQ9vebu+FLn2MIgUlIS99d8q3mNBedwY9XH\nvOHU/E/9VfThenyWg5j8rL/dRVSd6kGgk1T32ReMdUJNu32YXMacyPLPo7UGuqf1\nxHLyyyGEeFCCAkNsAUipmuUfwRSCmsMfxcnm0x6j35diLoLBm/8MaXN7qb+Kuctp\nzVvNch8CgYEApxPykZ08ESZ9uH19qbmhpwAuhCPeIuqYds9wCfc8z3JXYGIqn3zo\nbQdwmhPgnL6h1APglaogZmQzSlC9RRG0LSrGqWaETU1hSep6ppBSR23qikE1bIXi\nsmrKDDCPMkv4kVT01ySm3eUOkssEVWnWW2KzVGcGReDZd16Fyd/Sz7cCgYB8ghi8\nJgK/3TuwNNF/u9iL3zaJgfpVpBmTV4jlgaspv6Be7yyLvMet6FHNxWTHcjYNbVma\nESrNr/8+6c9H+i5maXcGvoQp8Fg2OnZB6Dt9mVLgE/iUeNQUkDSFqoQf69IwsKVX\nATAOtGMaKyFqIfrjbIpYz+D89uRBY+nApTwu+wKBgQCSsmt5cixuMXfPC1n52pAB\n316EZAzxmfWV+bXCw6hob8NVT5NKM5zRgBlfQyIRH3i6mVAzFMBiFYGD3CF53/Eq\neLaGdvAUuPEqUB+8qBy32Fbx+gnLS+z/0hxVwZVuon/Fj92Ue0uv4ZvL1Tu1a7X/\nwImbW1OAR81d1c3QkmQwtA==\n-----END PRIVATE KEY-----\n',
+          client_email: 'content@sovemykt.iam.gserviceaccount.com',
+          client_id: '116759792254885430704',
+          auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+          token_uri: 'https://oauth2.googleapis.com/token',
+          auth_provider_x509_cert_url:
+            'https://www.googleapis.com/oauth2/v1/certs',
+          client_x509_cert_url:
+            'https://www.googleapis.com/robot/v1/metadata/x509/content%40sovemykt.iam.gserviceaccount.com',
+        },
         filterNode: () => true,
         mapNode: node => node,
       },
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react'
+import { CartContextProvider } from './src/context/CartContext'
+import { StateContextProvider } from './src/context/StateContext'
+
+export const wrapRootElement = ({ element }) => (
+  <CartContextProvider>
+    <StateContextProvider>{element}</StateContextProvider>
+  </CartContextProvider>
+)
